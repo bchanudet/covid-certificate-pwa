e511c4ca82582f717f7a72d90b2e6dd0df975c4d
Benjamin Chanudet <hello@benjaminchanudet.com>
2021-07-16 23:28:32 +0200

    add guard to redirect straight to /list

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StraightToListGuard } from '../guards/straight-to-list.guard';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent},
+  { path: 'home', component: HomePageComponent, canActivate:[StraightToListGuard]},
   { path: '**', redirectTo: 'home'}
 ];
 
