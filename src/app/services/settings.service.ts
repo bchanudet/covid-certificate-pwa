146aced4b5f4489a935c2f4fd3fd281e8459afbc
Benjamin Chanudet <hello@benjaminchanudet.com>
2021-09-03 01:58:15 +0200

    store and fetches default cert setting

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, ReplaySubject } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
 import { I18nService } from '../i18n/i18n.service';
 import { StorageService } from './storage.service';
 
@@ -8,8 +8,9 @@ import { StorageService } from './storage.service';
 })
 export class SettingsService {
 
-  private schemeSub: ReplaySubject<'default' | 'light' | 'dark'> = new ReplaySubject(1);
-  private langSub: ReplaySubject<string> = new ReplaySubject(1);
+  private _schemeSub: ReplaySubject<'default' | 'light' | 'dark'> = new ReplaySubject(1);
+  private _langSub: ReplaySubject<string> = new ReplaySubject(1);
+  private _defaultCert: BehaviorSubject<string> = new BehaviorSubject<string>("");
 
   constructor(
     private storageSvc: StorageService,
@@ -24,21 +25,23 @@ export class SettingsService {
     }
     if(this.storageSvc.getPreferenceValue('language') !== ""){
       const lang = this.storageSvc.getPreferenceValue('language');
-      this.langSub.next(lang);
+      this._langSub.next(lang);
       this.i18nSvc.useLanguage(lang);
     }
     else{
-      this.langSub.next('default');
+      this._langSub.next('default');
     }
+
+    this._defaultCert.next(this.storageSvc.getPreferenceValue('defaultCert'));
   }
 
   // Scheme handling
   public get currentScheme$() : Observable<'default' | 'light' | 'dark'>{
-    return this.schemeSub.asObservable();
+    return this._schemeSub.asObservable();
   }
   public set currentScheme(newScheme: 'default' | 'light' | 'dark'){
     this.storageSvc.setPreferenceValue('scheme', newScheme);
-    this.schemeSub.next(newScheme);
+    this._schemeSub.next(newScheme);
   }
 
   // Language handling
@@ -46,12 +49,24 @@ export class SettingsService {
     return this.i18nSvc.getLanguages();
   }
   public get currentLanguage$() : Observable<string> {
-    return this.langSub.asObservable();
+    return this._langSub.asObservable();
   }
   public set currentLanguage(newLanguage: string){
     this.storageSvc.setPreferenceValue('language', newLanguage);
-    this.langSub.next(newLanguage);
+    this._langSub.next(newLanguage);
     this.i18nSvc.useLanguage(newLanguage);
+  }
+
+  // Default cert handling
+  public get defaultCert(): string{
+    return this._defaultCert.value;
+  }
+  public set defaultCert(newDefaultCert: string){
+    this.storageSvc.setPreferenceValue('defaultCert', newDefaultCert);
+    this._defaultCert.next(newDefaultCert);
+  }
+  public getStandaloneURL(): string{
+    return '/list'
   }
 
 
