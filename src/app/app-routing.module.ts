9e5b059f98bfbd001e0028c9f17a84d720671675
Benjamin Chanudet <hello@benjaminchanudet.com>
2021-09-03 01:57:52 +0200

    create guard to redirect to default certificate if there is one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { CertRoutingModule } from './content/cert/cert-routing.module';
 import { HomeRoutingModule } from './content/home/home-routing.module';
 import { ListRoutingModule } from './content/list/list-routing.module';
 import { SettingsRoutingModule } from './content/settings/settings-routing.module';
+import { HasNoDefaultCertGuard } from './guards/has-no-default-cert.guard';
 
 const routes: Routes = [
   {path:"", loadChildren : () => HomeRoutingModule, pathMatch: 'full'},
   {path:"new", loadChildren: () => AddRoutingModule},
-  {path:"list", loadChildren: () => ListRoutingModule},
+  {path:"list", loadChildren: () => ListRoutingModule, canActivate: [HasNoDefaultCertGuard]},
   {path:"cert", loadChildren: () => CertRoutingModule},
   {path:"settings", loadChildren: () => SettingsRoutingModule},
   {path: '**', redirectTo:'home'}
---
+++ src/app/guards/has-no-default-cert.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AppUpdateService } from '../services/app-update.service';
+import { SettingsService } from '../services/settings.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HasNoDefaultCertGuard implements CanActivate {
+
+  constructor(
+    private updateSvc: AppUpdateService,
+    private settingsSvc: SettingsService,
+    private router: Router
+  ){
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot
+  ): boolean | UrlTree {
+
+    const flag : string | null = route.queryParamMap.get("app");
+    if(flag === "true"){
+
+      this.updateSvc.setInApp(true);
+
+      if(this.settingsSvc.defaultCert !== ""){
+        return this.router.parseUrl("/cert/"+ this.settingsSvc.defaultCert);
+      }
+
+      console.log("has no default cert");
+      return true;
+    }
+    return true;
+  }
+
+}
